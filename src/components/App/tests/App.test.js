cf3db7cc025651d0ef14ade42576c9b3623a480d
Dan Kreiger <dan@dankreiger.com>
2018-03-11 20:09:54 +0100

    Add rendering test

--- src/components/App/tests/App.test.js
+++ src/components/App/tests/App.test.js
@@ -2,8 +2,15 @@ import React from 'react';
 import {shallow} from 'enzyme';
 import App from '../App';
 
-const app = shallow(<App />);
+let app;
+beforeEach(() => {
+  app = shallow(<App />);
+});
 
 it('renders without crashing', () => {
   expect(app).toMatchSnapshot();
+});
+
+it('renders <LoginForm /> component', () => {
+  expect(app.text()).toEqual("<LoginForm />");
 });
