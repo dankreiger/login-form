9252c01656bea8cb4999d7887dc6d0449d9bf25d
Dan Kreiger <dan@dankreiger.com>
2018-03-11 19:52:13 +0100

    Move tests into subdirectories

--- src/components/LoginForm/tests/LoginField.test.js
+++ src/components/LoginForm/tests/LoginField.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {shallow} from 'enzyme';
-import LoginField from './LoginField';
+import LoginField from '../LoginField';
 
 const loginfield = shallow(<LoginField glyph='user' />);
 
--- src/components/LoginForm/tests/LoginForm.test.js
+++ src/components/LoginForm/tests/LoginForm.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {shallow} from 'enzyme';
-import LoginForm from './LoginForm';
+import LoginForm from '../LoginForm';
 
 const loginform = shallow(<LoginForm />);
 
