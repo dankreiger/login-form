e528c3dd9e6e305a89bcb09410751f8126a6624d
Dan Kreiger <dan@dankreiger.com>
2018-03-11 19:21:55 +0100

    Do not ignore css for travis

---
+++ src/components/LoginForm/LoginForm.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import LoginForm from './LoginForm';
+
+const loginform = shallow(<LoginForm />);
+
+it('renders without crashing', () => {
+  expect(loginform).toMatchSnapshot();
+});
